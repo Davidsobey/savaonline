ad6f7b5b9dde36eaa5f7fb0a457a578afee01154
David <davidsobey@live.co.uk>
2019-05-05 08:32:47 +0200

    initial commit

---
+++ php/process-form.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_REQUEST['firstname'],$_REQUEST['email'])) {
+      
+    $firstname = $_REQUEST['firstname'];
+	$lastname = $_REQUEST['lastname'];
+    $email = $_REQUEST['email'];
+    $message = $_REQUEST['message'];
+    $message = $_REQUEST['message'];
+    $phone = $_REQUEST['phone'];
+
+
+      
+    // Set your email address where you want to receive emails. 
+    $to = 'info@aatautoassist.co.za';
+      
+    $subject = 'AAT:  Client Request has been made';
+    $headers = "From: ".$firstname." <".$email."> (".$phone.") \r\n";
+      
+    $send_email = mail($to,$subject,$message,$headers);
+      
+    echo ($send_email) ? 'success' : 'error';
+      
+}
+?>
